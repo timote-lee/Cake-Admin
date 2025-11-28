1d9833e941bff05fa573b4dc40ef8a168366335e
Timothy Lee <timothy@quasapp.com>
2025-11-28 14:09:48 +0800

    renamed project name

--- templates/layout/app.php
+++ templates/layout/app.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>JY Store Portal - <?= $this->fetch('title') ?></title>
+        <title>Cake Portal - <?= $this->fetch('title') ?></title>
         <meta charset="utf-8"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="<?= $this->request->getAttribute('csrfToken') ?>">
@@ -23,7 +23,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                         </label>
 
-                        <span class="font-bold align-middle lg:px-3">JY Store Portal</span>
+                        <span class="font-bold align-middle lg:px-3">Cake Portal</span>
                     </div>
                         
                     <div class="flex-none">
