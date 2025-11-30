44dfbd1d0d24b0b7542a3969f4bfc77529e8dfee
Timothy Lee <timothy@quasapp.com>
2025-11-30 15:24:29 +0800

    compiled libraries with Tailwind CLI and Prepos instead

--- templates/layout/auth.php
+++ templates/layout/auth.php
@@ -6,15 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="<?= $this->request->getAttribute('csrfToken') ?>">
 
-        <!-- CSS libraries -->
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/daisyui@5" type="text/css">
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer">
-
-        <!-- custom CSS -->
+        <!-- CSS -->
         <link rel="stylesheet" href="<?= $this->Url->css('auth.css'); ?>">
     </head>
 
-    <body>
+    <body data-theme="light">
         <div class="main">
             <div class="body">
                 <?= $this->fetch('main') ?>
@@ -26,8 +22,9 @@
         <script src="//code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         
-        <!-- custom JS -->
-        <script src="<?= $this->Url->script('base.js'); ?>"></script>
+        <!-- JS -->
+        <script src="<?= $this->Url->script('auth.js'); ?>"></script>
+
         <?= $this->fetch('js') ?>
     </body>
 </html>
